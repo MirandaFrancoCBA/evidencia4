c1ac83fce608006870d7b2fc8413da9aa3c7f0f6
arcadex007 <arcadex007@gmail.com>
2024-09-06 20:19:48 +0000

    probando la maquina

--- test_maquina.py
+++ test_maquina.py
@@ -1,5 +1,4 @@
 """Aqui probare la maquina"""
-
 from CortadoraVidrio import CortadoraVidrio
 
 def test_cortar_figura():
@@ -22,3 +21,62 @@ def test_str():
     assert str(cortadora) == "Forma seleccionada: Cuadrado, Color: Rojo, Frase: Hola Mundo"
 
 
+pieza1 = CortadoraVidrio()
+pieza1.cortar_figura("Cuadrado")
+pieza1.grabar("Hola Mundo")
+pieza1.pintar("Rojo")
+print(pieza1)
+
+pieza2 = CortadoraVidrio()
+pieza2.cortar_figura("Triangulo")
+pieza2.grabar("Soy un triangulo")
+pieza2.pintar("Azul")
+print(pieza2)
+
+pieza3 = CortadoraVidrio()
+pieza3.cortar_figura("Circulo")
+pieza3.grabar("659135469")
+pieza3.pintar("Verde")
+print(pieza3) 
+
+pieza4 = CortadoraVidrio()
+pieza4.cortar_figura("Rectangulo")
+pieza4.grabar("Soy un rectangulo")
+pieza4.pintar("Amarillo")
+print(pieza4)
+
+pieza5 = CortadoraVidrio()
+pieza5.cortar_figura("Rombo")
+pieza5.grabar("Soy un rombo")
+pieza5.pintar("Naranja")
+print(pieza5)
+
+pieza6 = CortadoraVidrio()
+pieza6.cortar_figura("Pentagono")
+pieza6.grabar("CBA 2024")
+pieza6.pintar("Morado")
+print(pieza6)
+
+pieza7 = CortadoraVidrio()
+pieza7.cortar_figura("Hexagono")
+pieza7.grabar("CBA 2024")
+pieza7.pintar("Rosa")
+print(pieza7)
+
+pieza8 = CortadoraVidrio()
+pieza8.cortar_figura("Cuadrado")
+pieza8.grabar("Soy un cuadrado")
+pieza8.pintar("Violeta")
+print(pieza8)
+
+pieza9 = CortadoraVidrio()
+pieza9.cortar_figura("Triangulo")
+pieza9.grabar("Soy un triangulo")
+pieza9.pintar("Celeste")
+print(pieza9)
+
+pieza10 = CortadoraVidrio()
+pieza10.cortar_figura("Circulo")
+pieza10.grabar("CBA 2024")
+pieza10.pintar("Turquesa")
+print(pieza10)
